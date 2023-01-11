1adc70d0896e22d119537848eae6d0ed5ae7fba2
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2023-01-11 15:50:49 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -381,22 +381,21 @@ for name, frame, framew in zip(names,framelist, framelistw):
             #(framew['Dec_EMA50'].iloc[-1]=='Sell'):
             #(framew['Trend MACD'].iloc[-1]=='Sell' or framew['Dec_EMA50'].iloc[-1]=='Sell'):
                 if option2 == 'breakout':
-                   if (frame['Decision Super'].iloc[-h1]=='Sell' or frame['Decision Super2'].iloc[-h1]=='Sell' or frame['Decision Super2'].iloc[-h1]=='Sell'\
+                   if (frame['Decision Super2'].iloc[-h1]=='Sell' or frame['Decision Super2'].iloc[-h1]=='Sell' or frame['Decision Super2'].iloc[-h1]=='Sell'\
                    or frame['EMA50_cross'].iloc[-h1]=='Sell' or frame['EMA50_cross'].iloc[-h1]=='Sell')\
                    and (frame['Close'].iloc[-h1]<frame['sup4'].iloc[-h1] or frame['Close'].iloc[-h1]<frame['sup6'].iloc[-h1]):
                              sira +=1
                              expander('breakout')
+                   elif (framew['Decision Super2'].iloc[-2]=='Sell' or framew['EMA50_cross'].iloc[-h1]=='Sell')\
+                   and (framew['Close'].iloc[-h1]<frame['sup4'].iloc[-h1] or framew['Close'].iloc[-h1]<framew['sup6'].iloc[-h1]):
+                            sira +=1
+                            expander('week breakout')
                 if option2=='pullback':
-                   if (frame['Decision Super'].iloc[-h1]=='Sell2' or frame['Decision Super2'].iloc[-h1]=='Sell2' or frame['Decision Super3'].iloc[-h1]=='Sell2'\
+                   if (frame['Decision Super2'].iloc[-h1]=='Sell2' or frame['Decision Super2'].iloc[-h1]=='Sell2' or frame['Decision Super3'].iloc[-h1]=='Sell2'\
                    or frame['EMA50_cross'].iloc[-h1]=='Sell2' or frame['EMA50_cross'].iloc[-h1]=='Sell2' or frame['EMA200_cross'].iloc[-h1]=='Sell2')\
                    and (frame['Close'].iloc[-h1]<frame['sup4'].iloc[-h1] and frame['Close'].iloc[-h1]<frame['sup6'].iloc[-h1] and frame['Dec_EMA50'].iloc[-1]=='Sell'):
                             sira +=1
                             expander('pullback')
-                if option2 == 'week':
-                   if (framew['Decision Super2'].iloc[-2]=='Sell' or framew['EMA50_cross'].iloc[-h1]=='Sell')\
-                   and (framew['Close'].iloc[-h1]<frame['sup4'].iloc[-h1] or framew['Close'].iloc[-h1]<framew['sup6'].iloc[-h1]):
-                            sira +=1
-                            expander('week breakout')
                    elif (framew['Decision Super2'].iloc[-2]=='Sell2' or framew['Decision Super3'].iloc[-2]=='Sell2')\
                    and (framew['Close'].iloc[-h1]<frame['sup4'].iloc[-h1] or framew['Close'].iloc[-h1]<framew['sup6'].iloc[-h1]):
                             sira +=1
