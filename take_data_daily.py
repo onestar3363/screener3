a15d657bad95c4e31dee3a225a8f425ddc8afb57
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2023-01-11 15:57:40 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -371,7 +371,7 @@ for name, frame, framew in zip(names,framelist, framelistw):
                    and (frame['Close'].iloc[-h1]<frame['sup4'].iloc[-h1] or frame['Close'].iloc[-h1]<frame['sup6'].iloc[-h1]):
                              sira +=1
                              expander('breakout')
-                   elif (framew['Decision Super2'].iloc[-2]=='Sell' or framew['EMA50_cross'].iloc[-h1]=='Sell')\
+                   elif (framew['Decision Super2'].iloc[-2]=='Sell' or framew['Decision Super3'].iloc[-2]=='Sell')\
                    and (framew['Close'].iloc[-h1]<frame['sup4'].iloc[-h1] or framew['Close'].iloc[-h1]<framew['sup6'].iloc[-h1]):
                             sira +=1
                             expander('week breakout')
