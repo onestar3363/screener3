7d6b728519530a4f0b2aa5b9a8d4240ecbb45f0c
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2023-01-16 08:22:57 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -316,7 +316,7 @@ for name, frame, framew in zip(names,framelist, framelistw):
             #or framew['Close'].iloc[-1]>framew['sup2'].iloc[-1] or framew['Close'].iloc[-1]>framew['sup4'].iloc[-1])):
            
                 if option2 == 'breakout':
-                   if (frame['EMA50_cross'].iloc[-h1]=='Buy' or frame['EMA50_cross'].iloc[-h1]=='Buy' or frame['Decision Super2'].iloc[-h1]=='Buy')\
+                   if (frame['EMA20_cross'].iloc[-h1]=='Buy' or frame['EMA50_cross'].iloc[-h1]=='Buy' or frame['Decision Super2'].iloc[-h1]=='Buy')\
                    and (frame['Close'].iloc[-h1]>frame['sup4'].iloc[-h1] or frame['Close'].iloc[-h1]>frame['sup6'].iloc[-h1]):
                    #(frame['Decision Super2'].iloc[-h1]=='Buy' or frame['Decision Super2'].iloc[-h1]=='Buy' or frame['Decision Super3'].iloc[-h1]=='Buy'\
                             sira +=1
