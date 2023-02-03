5c0b7876a18c3ada0987791e563754c6e5289599
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2023-02-03 23:05:48 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -318,8 +318,8 @@ for name, frame, framew in zip(names,framelist, framelistw):
             #or framew['Close'].iloc[-1]>framew['sup2'].iloc[-1] or framew['Close'].iloc[-1]>framew['sup4'].iloc[-1])):
            
                 if option2 == 'breakout':
-                   if (frame['EMA20_cross'].iloc[-h1]=='Buy' or frame['EMA50_cross'].iloc[-h1]=='Buy' or frame['Decision Super2'].iloc[-h1]=='Buy')\
-                   and frame['Close'].iloc[-h1]>frame['sup6'].iloc[-h1]:
+                   if (frame['EMA50_cross'].iloc[-h1]=='Buy' or frame['EMA50_cross'].iloc[-h1]=='Buy' or frame['Decision Super2'].iloc[-h1]=='Buy')\
+                   and (frame['Dec_EMA50'].iloc[-h1]=='Buy' or frame['Close'].iloc[-h1]>frame['sup6'].iloc[-h1] or framew['Close'].iloc[-h1]>framew['sup'].iloc[-h1]):
                    #(frame['Decision Super2'].iloc[-h1]=='Buy' or frame['Decision Super2'].iloc[-h1]=='Buy' or frame['Decision Super3'].iloc[-h1]=='Buy'\
                             sira +=1
                             expander('breakout')
