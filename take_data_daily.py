047fcb791cc5f6eeb8efe6a9ffe3b955c0bff77c
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2023-02-04 00:19:11 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -243,8 +243,8 @@ def get_figures(frame,r):
          mode='markers', marker=dict(size=3,color='green'), 
          name='Supertrend1'))
     
-    fig.add_trace(go.Scatter(x=frame['Date'].tail(5), 
-         y=framew['sup2'].tail(1),
+    fig.add_trace(go.Scatter(x=frame['Date'].tail(20), 
+         y=framew['sup2'].tail(20),
          opacity=0.7,
          mode='markers', marker=dict(size=3,color='red'), 
          name='Supertrend1'))
