d6bdf15bd1a218449e8431fcee546b77365b92da
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2023-02-04 23:09:24 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -342,7 +342,8 @@ for name, frame, framew in zip(names,framelist, framelistw):
                    if ( frame['Consolidating2'].iloc[-h1]=='Yes' or frame['Consolidating3'].iloc[-h1]=='Yes')\
                    and (frame['Dec_EMA50'].iloc[-h1]=='Buy' or frame['Dec_EMA20'].iloc[-h1]=='Buy')\
                    and (frame['Close'].iloc[-h1]>frame['sup6'].iloc[-h1])\
-                   and frame['Dec_EMA200'].iloc[-h1]=='Buy'\
+                   and (frame['Dec_EMA50'].iloc[-h1]=='Buy' and frame['Close'].iloc[-h1]>frame['sup6'].iloc[-h1])\
+                   and (framew['Close'].iloc[-1]>framew['sup2'].iloc[-1] or framew['Close'].iloc[-1]>framew['sup4'].iloc[-1])\
                    and frame['Trend MACD'].iloc[-1]=='Buy':
                             sira +=1
                             expander('consolidating') 
